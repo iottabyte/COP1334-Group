5751345680e59d4ec8caa8475c4dde909ca6b09c
透明 気美 <72657181+iottabyte@users.noreply.github.com>
2021-04-05 15:04:47 -0400

    made framework for source file

---
+++ Lost in Everglades/Lost in Everglades/evergladesRPG.cpp
@@ -0,0 +1,44 @@
+/*
+	Brian (last name? lmao sorry!), Jayde Shaw, Kim Claros
+
+	evergladesRPG.cpp
+
+	[ desc ]
+
+	I:	initial menu choice, game decisions
+
+	P:
+
+	O:	5 x 5 cell map (maybe expand on this?)
+*/
+
+// header files
+#include <iostream>
+using namespace std;
+
+// function prototypes
+
+
+
+int main()
+{
+	// constants and variables
+
+
+	// intro
+
+	// pretty sure driving menu with a switch-case, but idk yet
+
+
+
+	return 0;
+}
+
+/*
+	function 1
+*/
+
+
+/*
+	function 2
+*/
