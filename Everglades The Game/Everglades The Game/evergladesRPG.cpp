bd6c0bba319a1ea872972bc01a40f43220998a8b
透明 気美 <72657181+iottabyte@users.noreply.github.com>
2021-04-05 16:31:43 -0400

    started the menu

--- Everglades The Game/Everglades The Game/evergladesRPG.cpp
+++ Everglades The Game/Everglades The Game/evergladesRPG.cpp
@@ -27,10 +27,36 @@ int main()
 	int playerChoice;
 
 	// intro
+	cout << "\n\t\tLost in the Everglades" << endl;
 
 	// pretty sure driving menu with a switch-case, but idk yet
+	do
+	{
+		// display menu
+		cout << "\n[just pick one]" << endl
+			<< "\n\t1 - See Rules" << endl
+			<< "\t2 - Play Game" << endl
+			<< "\t3 - Quit" << endl
+			<< "\n[so what is it]: ";
+		cin >> playerChoice;
 
+		switch (playerChoice)
+		{
 
+		case 1:		// rules
+			;
+			break;
+		case 2:		// let's play a game
+			;
+			break;
+		case 3:		// quit
+			cout << "\n[some kind of quit message]" << endl;
+
+		default:	// error
+			cout << "ERROR: Invalid selection. Please try again." << endl;
+
+		}
+	} while (playerChoice != 3);
 
 	return 0;
 }
